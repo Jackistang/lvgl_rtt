16292683a742973bdacd4f7753d03d7dea205dc0
jackis <tangjia.jackis@qq.com>
2020-12-31 00:29:03 +0800

    add port framework

---
+++ inc/lvgl.h
@@ -0,0 +1,14 @@
+#ifndef LVGL_RTT_LVGL_H
+#define LVGL_RTT_LVGL_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "../lvgl/lvgl.h"
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  /* LVGL_RTT_LVGL_H */
